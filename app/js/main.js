6e1fa90b32d9edc5ac40422771ecfbd392ca1da8
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-12-26 21:30:35 +0300

    adaptive home page, hotfixes, etc

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,8 @@
 import {modalQuote} from "./components/modalQuote.js";
+import {setupMobileMenu} from "./components/mobileMenu.js";
 
 modalQuote();
-
-/*
-const html = document.documentElement;
-const menuBtn = document.querySelector('.menu-btn');
-const headerNav = document.querySelector('.header__mobile');
-const anchors = document.querySelectorAll('a.header__link.mobile');
-
-function scrollToTarget(targetId) {
-  const targetSection = document.querySelector(targetId);
-  if (targetSection) {
-    html.classList.remove('active');
-    headerNav.classList.remove('active');
-    menuBtn.classList.remove('active');
-    setTimeout(() => {
-      const targetOffset = targetSection.offsetTop - 50;
-      window.scrollTo({top: targetOffset, behavior: 'smooth'});
-    }, 400);
-  }
-}
-
-function handleAnchorClick(event) {
-  event.preventDefault();
-  const href = this.getAttribute('href');
-  const hrefParts = href.split('#');
-  if (hrefParts.length === 2) {
-    const targetId = '#' + hrefParts[1];
-    scrollToTarget(targetId);
-  }
-}
-
-for (const anchor of anchors) {
-  anchor.addEventListener('click', handleAnchorClick);
-  anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
-}
-
-document.querySelectorAll(".desktop").forEach(link => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    const targetId = link.getAttribute("href").slice(1);
-    const targetSection = document.getElementById(targetId);
-
-    if (targetSection) {
-      const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY - 40;
-
-      window.scrollTo({
-        top: offsetTop,
-        behavior: "smooth"
-      });
-    }
-  });
-});*/
+setupMobileMenu();
 
 new Swiper('.leading__swiper', {
   speed: 800,
