05d3bf9694c0a6761a1b62659976fc5667ab03c0
Paha777 <159909421+PavelBoyko07@users.noreply.github.com>
2025-08-20 12:35:22 +0300

    выполнен адаптив для: - страницы с кейсами - страницы с планами (plans.html)
    Исправлены также некоторые некорректности стилей

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,38 @@ import {setupMobileMenu} from "./components/mobileMenu.js";
 modalQuote();
 setupMobileMenu();
 
-new Swiper('.leading__swiper', {
-  speed: 800,
-  spaceBetween: 10,
-  slidesPerView: 'auto',
-  scrollbar: {
-    el: '.leading__swiper-scrollbar',
-    draggable: true
-  }
-})
+new Swiper('.swiper-plans', {
+  loop: false,
+  navigation: {
+    nextEl: '.pricing__btn--next',
+    prevEl: '.pricing__btn--prev',
+  },
+  breakpoints: {
+    1260: {
+      slidesPerView: 3,
+      spaceBetween: 7.5
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 7.5
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    425: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      freeMode: {
+        enabled: true
+      },
+    },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+      freeMode: {
+        enabled: false
+      },
+    }
+  },
+});
