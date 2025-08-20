4f2cc6b468341ab03f3186f7b3a86eeff416c93f
Paha777 <159909421+PavelBoyko07@users.noreply.github.com>
2025-08-20 23:11:32 +0300

    Сделана оптимизация изображений. Теперь заместо веса в несколько мегабайт, папка images весит всего 1.48 мегабайт
    Вернулось модальное окно статистики каналов
    
    Сделан адаптив для мобильного меню. Теперь меню сделано на широких устройствах с затемнением справа (при клике на пустоту - меню закрывается)

--- app/js/components/mobileMenu.js
+++ app/js/components/mobileMenu.js
@@ -1,25 +1,33 @@
 export function setupMobileMenu() {
   const html = document.documentElement;
+  const body = document.body;
   const menuBtn = document.querySelector('.menu-btn');
   const headerMobile = document.querySelector('.header__mobile');
+  const header = document.querySelector('.header'); // добавляем основной header
   const anchors = document.querySelectorAll('a.mobile');
 
   menuBtn.addEventListener('click', () => {
     menuBtn.blur();
     html.classList.toggle('active');
+    body.classList.toggle('modal-active');
     menuBtn.classList.toggle('active');
     headerMobile.classList.toggle('active');
   });
 
+  function closeMenu() {
+    html.classList.remove('active');
+    body.classList.remove('modal-active');
+    menuBtn.classList.remove('active');
+    headerMobile.classList.remove('active');
+  }
+
   function scrollToTarget(targetId) {
     const targetSection = document.querySelector(targetId);
     if (targetSection) {
-      html.classList.remove('active');
-      headerMobile.classList.remove('active');
-      menuBtn.classList.remove('active');
+      closeMenu();
       setTimeout(() => {
         const targetOffset = targetSection.offsetTop - 25;
-        window.scrollTo({top: targetOffset, behavior: 'smooth'});
+        window.scrollTo({ top: targetOffset, behavior: 'smooth' });
       }, 700);
     }
   }
@@ -36,6 +44,13 @@ export function setupMobileMenu() {
 
   for (const anchor of anchors) {
     anchor.addEventListener('click', handleAnchorClick);
-    anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
+    anchor.addEventListener('touchstart', handleAnchorClick, { passive: true });
   }
+
+  // Закрытие меню при клике вне header/header__mobile
+  document.addEventListener('click', (event) => {
+    if (!header.contains(event.target) && !headerMobile.contains(event.target) && !menuBtn.contains(event.target)) {
+      closeMenu();
+    }
+  });
 }
